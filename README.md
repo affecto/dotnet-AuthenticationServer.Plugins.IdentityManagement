3e64429e0d6cedfdbbcc8db24e01b904abf61487
johannesvaltonen <johannes.valtonen@affecto.com>
2016-06-15 16:20:14 +0300

    Moved code from Authentication Server repository.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Authentication Server Identity Management Plugin
+
+Plugin for authenticating users against Affecto Identity Management.
+
+## NuGet packages
+
+* **Affecto.AuthenticationServer.Claims**
+  * Build claims for authenticated user and construct a user object from claims.
+  * NuGet: https://www.nuget.org/packages/Affecto.AuthenticationServer.Claims
+
+### Build status
+
+[![Build status](https://ci.appveyor.com/api/projects/status/14v5fubj1wevbqj2?svg=true)](https://ci.appveyor.com/project/affecto/dotnet-authenticationserver)
---
+++ Source/AuthenticationServer.Plugins.IdentityManagement/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Authentication Server - Identity Management Plugin")]
+[assembly: AssemblyProduct("Affecto Authentication Server")]
+[assembly: AssemblyCompany("Affecto")]
+
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
+
+[assembly: InternalsVisibleTo("Affecto.AuthenticationServer.Plugins.IdentityManagement.Tests")]
+[assembly: InternalsVisibleTo("Affecto.AuthenticationServer.Plugins.IdentityManagement.ConfigurationTests")]
